493e7861ecbf967e7e8001eb63ce672f0a873aa8
dev-0x740x690x6d <187819120+dev-0x740x690x6d@users.noreply.github.com>
2025-05-14 05:28:15 +0000

    Added asteroid field.

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     pygame.init()
@@ -9,28 +11,39 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
-    player_screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     background_color = "#000000" #black
 
-    updatable_group = pygame.sprite.Group()
-    drawable_group = pygame.sprite.Group()
-    Player.containers = (updatable_group, drawable_group)
+    game_clock = pygame.time.Clock()
+    dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (updatable, drawable, asteroids)
+    AsteroidField.containers = (updatable)
 
     player_x = SCREEN_WIDTH / 2
     player_y = SCREEN_HEIGHT / 2
     player = Player(player_x, player_y)
+    asteroid_field = AsteroidField()
     
-    game_clock = pygame.time.Clock()
-    dt = 0
+    
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        updatable_group.update(dt)
-        player_screen.fill(background_color)
-        for draw_object in drawable_group:
-            draw_object.draw(player_screen)
+        screen.fill(background_color)
+        
+        for draw_object in drawable:
+            draw_object.draw(screen)
+
+        for update_object in updatable:
+            update_object.update(dt)
+
         pygame.display.flip()
         dt = game_clock.tick(60) / 1000
         
--- player.py
+++ player.py
@@ -9,8 +9,6 @@ class Player(CircleShape):
         self.radius = PLAYER_RADIUS
         self.rotation = 0
 
-    containers = ()
-
     # in the player class
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
